26dd10b02bf115ad927e58c141b26251729639bc
omprakashjadhav <omprakashjadhav87@gmail.com>
2023-07-29 10:41:43 +0530

    updated

---
+++ 03_varibale/Default/02command_line_variable/main.tf
@@ -0,0 +1,41 @@
+#
+#
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.10.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
+
+resource "local_file" "Geniric_hardcoded" {
+  filename = "/mnt/hardcoded.txt"
+  content  = "hi from ip 1.2.3.4.5"
+}
+
+resource "local_file" "Geniric_variable" {
+  #key           ="value"
+  filename = var.filename_str
+  content = var.content_str
+
+}
+
+resource "local_file" "Geniric_list" {
+  #key                  ="value"
+  filename              = var.file_list[0]
+  content               = var.file_list[1]
+  file_permission       = var.file_list[2]
+
+}
+
+resource "local_file" "Map_list" {
+  #key                  ="value"
+ filename                =var.file_map["filename"]
+ content                 =var.file_map["content"]
+ file_permission         =var.file_map["file_permission"]
+}
