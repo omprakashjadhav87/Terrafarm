5910cce40a104a4680b902ff2764d9d4443df1b2
omprakashjadhav <omprakashjadhav87@gmail.com>
2023-08-03 12:06:47 +0530

    updated

--- 04-cross-resourese-attribute/main.tf
+++ 04-cross-resourese-attribute/main.tf
@@ -1,5 +1,6 @@
 resource "aws_instance" "generic_ec2"{
     ami ="ami-0f34c5ae932e6f0e4"
-    availability_zone "us-east-1c"
-    
+    availability_zone= "us-east-1c"
+    instance_type = "t2.micro"
+
 }
