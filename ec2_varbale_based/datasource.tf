18752380d41ccd91f5d96fba4414aa06e2743ae3
omprakashjadhav <omprakashjadhav87@gmail.com>
2023-09-02 14:02:42 +0530

    updated

--- ec2_varbale_based/datasource.tf
+++ ec2_varbale_based/datasource.tf
@@ -2,7 +2,7 @@ data "aws_ami" "latest_amazon_ami" {
     most_recent = true
     owners = [ "amazon" ]
 filter {
-  name = "Name"
+  name = "name"
   values = [ "al2023-ami-2023.1.*.0-kernel-6.1-x86_64" ]
 }  
 }
