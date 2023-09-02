e44cadc03f0ee174c59516042527bb0d27945c3c
omprakashjadhav <omprakashjadhav87@gmail.com>
2023-09-02 14:04:30 +0530

    updated

--- ec2_varbale_based/ec2_backend.tf
+++ ec2_varbale_based/ec2_backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-prac-test0124"
-    key    = "ec2/ec2.tfstate"
+    key    = "ec2/ec2webserver.tfstate"
     region = "us-east-1"
     profile = "default"
   }
