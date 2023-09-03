01834f524ff1804e019d6e0310f70b4eec67af4f
omprakashjadhav <omprakashjadhav87@gmail.com>
2023-09-03 12:54:58 +0530

    updated

--- ec2_webserver/sg.tf
+++ ec2_webserver/sg.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "genericwebserver_sg" {
-    vpc_id = "vpc-086945c958c323de0"
+    vpc_id = data.terraform_remote_state.vpc_details.outputs.ekrushi_dev_vpc_id
     name = "webserver"
     description = "allow web trafic"
     tags = {
