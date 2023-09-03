e981034007860e3250f25c7081634779d4fd0327
omprakashjadhav <omprakashjadhav87@gmail.com>
2023-09-03 12:32:36 +0530

    updated

--- ec2_webserver/sg.tf
+++ ec2_webserver/sg.tf
@@ -1,4 +1,4 @@
-resource "aws_security_group" "webserver_sg" {
+resource "aws_security_group" "genericwebserver_sg" {
     vpc_id = data.terraform_remote_state.vpc_details.outputs.vpc_id
     name = "webserver"
     description = "allow web trafic"
@@ -11,20 +11,20 @@ resource "aws_security_group" "webserver_sg" {
     from_port =0
     to_port    =0
     protocol  ="-1"
-    cidir_blocks =["0.0.0.0/0"]
+    cidr_blocks =["0.0.0.0/0"]
 }
     ingress {
         description  ="allow web trafic on 80 port"
         from_port = 80
         to_port    = 80
         protocol  ="tcp"
-        cidir_blocks =["0.0.0.0/0"]
+        cidr_blocks =["0.0.0.0/0"]
     }
     ingress {
         description  ="allow web trafic on 22 port"
         from_port = 22
         to_port    = 22
         protocol  ="tcp"
-        cidir_blocks =["0.0.0.0/0"]
+        cidr_blocks =["0.0.0.0/0"]
     }
 }
