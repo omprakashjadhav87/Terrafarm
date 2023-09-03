4abc6ed868899b2768c404f0b28bb31fb1ff8084
omprakashjadhav <omprakashjadhav87@gmail.com>
2023-09-03 18:54:17 +0530

    updated

--- variable_based_vpc/remote_backend.tf
+++ variable_based_vpc/remote_backend.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "s3" {
     bucket = "terraform-prac-test012"
-    key    = "vpc/vpc.tfstate"
+    workspace_key_prefix = "vpc"
+    key                  = "vpc.tfstate"
     region = "us-east-1"
     profile = "default"
   }
