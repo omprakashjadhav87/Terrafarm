ab81b73c01a36450647cc48698b9bb7032aa763b
omprakashjadhav <omprakashjadhav87@gmail.com>
2023-09-03 12:28:53 +0530

    updated

--- ec2_webserver/sg.tf
+++ ec2_webserver/sg.tf
@@ -9,21 +9,21 @@ resource "aws_security_group" "webserver_sg" {
   
   egress {
     from_port =0
-    to_prt    =0
+    to_port    =0
     protocol  ="-1"
     cidir_blocks =["0.0.0.0/0"]
 }
     ingress {
         description  ="allow web trafic on 80 port"
         from_port = 80
-        to_prt    = 80
+        to_port    = 80
         protocol  ="tcp"
         cidir_blocks =["0.0.0.0/0"]
     }
     ingress {
         description  ="allow web trafic on 22 port"
         from_port = 22
-        to_prt    = 22
+        to_port    = 22
         protocol  ="tcp"
         cidir_blocks =["0.0.0.0/0"]
     }
