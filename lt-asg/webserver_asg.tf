f339e58e00bdbac7ab8bcc87856d4e518062048b
omprakashjadhav <omprakashjadhav87@gmail.com>
2023-09-05 17:15:08 +0530

    updated

--- lt-asg/webserver_asg.tf
+++ lt-asg/webserver_asg.tf
@@ -4,7 +4,7 @@ resource "aws_autoscaling_group" "webserver_asg" {
     name = var.asg_name
     max_size =var.max_size
     min_size = var.min_size
-    desireddesired_capacity = var.desired_capacity
+    desired_capacity = var.desired_capacity
     vpc_zone_identifier = [data.terraform_remote_state.vpc_details.outputs.ekrushi-web_pub_subnet_1a_id,data.terraform_remote_state.vpc_details.outputs.ekrushi-web_pub_subnet_1b_id]
     launch_template {
       id = aws_launch_template.generic_lt.id
