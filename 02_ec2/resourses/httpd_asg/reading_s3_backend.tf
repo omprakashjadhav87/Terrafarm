e91314824ab07f95d0cbfaa16731b0fbf8a3b69b
omprakashjadhav <omprakashjadhav87@gmail.com>
2023-09-06 15:08:08 +0530

    updated

--- 02_ec2/resourses/httpd_asg/reading_s3_backend.tf
+++ 02_ec2/resourses/httpd_asg/reading_s3_backend.tf
@@ -5,8 +5,8 @@ data "terraform_remote_state" "vpc_details" {
     backend = "s3"
     config = {
       bucket = "terraform-prac-test012"
-      workspace_key_prefix = "ec2"
-    key                  ="vpc/${terraform.workspace}/vpc.tfstate"
+      workspace_key_prefix = "lt"
+    key                  = "webserver_httpd_lt.tfstate"
       region = "us-east-1"
       profile = "default"
     }
