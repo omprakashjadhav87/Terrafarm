798fce0c951e1538ca80693771cd8699727db374
omprakashjadhav <omprakashjadhav87@gmail.com>
2023-09-08 12:31:13 +0530

    updated code

--- 01_vpc/variable_based_vpc/remote_backend.tf
+++ 01_vpc/variable_based_vpc/remote_backend.tf
@@ -6,4 +6,10 @@ terraform {
     region = "us-east-1"
     profile = "default"
   }
+
+
+
 }
+
+
+# s3//terraform-prac-test012 /VPC/stage/vpc.tfstate
--- 02_ec2/resourses/httpd_asg/reading_s3_backend.tf
+++ 02_ec2/resourses/httpd_asg/reading_s3_backend.tf
@@ -5,11 +5,10 @@ data "terraform_remote_state" "vpc_details" {
     backend = "s3"
     config = {
       bucket = "terraform-prac-test012"
-      workspace_key_prefix = "lt"
-    key                  = "webserver_httpd_lt.tfstate"
+      key                 = "vpc/${terraform.workspace}/vpc.tfstate"
       region = "us-east-1"
       profile = "default"
     }
-
- 
 }
+
+# s3//terraform-prac-test012 /VPC/stage/vpc.tfstate
